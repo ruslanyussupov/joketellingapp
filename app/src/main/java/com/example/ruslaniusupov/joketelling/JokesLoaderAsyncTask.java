1f39809a4be1d4be14c6e123cae2b8b280538d61
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-05-26 18:50:21 +0300

    Fetch jokes from backend

--- app/src/main/java/com/example/ruslaniusupov/joketelling/JokesLoaderAsyncTask.java
+++ app/src/main/java/com/example/ruslaniusupov/joketelling/JokesLoaderAsyncTask.java
@@ -1,10 +1,17 @@
 package com.example.ruslaniusupov.joketelling;
 
 import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.example.ruslaniusupov.backend.myApi.MyApi;
 import com.example.ruslaniusupov.displayjoke.model.Joke;
-import com.example.ruslaniusupov.jokes.DataProvider;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
+import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.List;
 
@@ -14,7 +21,11 @@ public class JokesLoaderAsyncTask extends AsyncTask<Void, Void, List<Joke>> {
         void OnLoaded(List<Joke> jokes);
     }
 
+    private static final String LOG_TAG = JokesLoaderAsyncTask.class.getSimpleName();
+
     private final WeakReference<OnLoadedListener> listenerWeakReference;
+    private static MyApi mMyApiService;
+
 
     public JokesLoaderAsyncTask(WeakReference<OnLoadedListener> listenerWeakReference) {
         this.listenerWeakReference = listenerWeakReference;
@@ -22,7 +33,42 @@ public class JokesLoaderAsyncTask extends AsyncTask<Void, Void, List<Joke>> {
 
     @Override
     protected List<Joke> doInBackground(Void... voids) {
-        return Utils.getJokes(DataProvider.getJokesJson());
+
+        if (mMyApiService == null) {
+            // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            mMyApiService = builder.build();
+
+        }
+
+        String json = null;
+        List<Joke> result = null;
+
+        try {
+            json =  mMyApiService.getJokesJson().execute().getJson();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Can't get data from API Service. " + e.getMessage());
+        }
+
+        if (!TextUtils.isEmpty(json)) {
+            result = Utils.getJokes(json);
+        }
+
+        return result;
+
     }
 
     @Override
--- jokes/src/main/java/com/example/ruslaniusupov/jokes/DataProvider.java
+++ jokes/src/main/java/com/example/ruslaniusupov/jokes/DataProvider.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 public class DataProvider {
 
