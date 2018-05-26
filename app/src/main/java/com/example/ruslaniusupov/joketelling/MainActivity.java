160afe8f888838ae40d60703a130d7e46dcfd56e
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-05-26 14:23:07 +0300

    Add paid and free flavors

--- app/src/main/java/com/example/ruslaniusupov/joketelling/MainActivity.java
+++ app/src/main/java/com/example/ruslaniusupov/joketelling/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.ruslaniusupov.joketelling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.tell_joke_btn)
+    public void tellJoke(View view) {
+
+    }
+
 }
