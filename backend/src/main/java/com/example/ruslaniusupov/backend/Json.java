f5a5d7a52e3b0bab540a231eeeee93979f56025d
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-05-26 18:30:35 +0300

    Add backend

---
+++ backend/src/main/java/com/example/ruslaniusupov/backend/Json.java
@@ -0,0 +1,15 @@
+package com.example.ruslaniusupov.backend;
+
+public class Json {
+
+    private final String json;
+
+    public Json(String json) {
+        this.json = json;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+}
---
+++ backend/src/main/java/com/example/ruslaniusupov/backend/MyBean.java
@@ -0,0 +1,15 @@
+package com.example.ruslaniusupov.backend;
+
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+
+}
