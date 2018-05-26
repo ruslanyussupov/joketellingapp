90d92b2ae787b6c01f2e2354b820e3a119cf8941
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-05-26 15:13:08 +0300

    Add display joke library

--- app/src/main/java/com/example/ruslaniusupov/joketelling/MainActivity.java
+++ app/src/main/java/com/example/ruslaniusupov/joketelling/MainActivity.java
@@ -1,19 +1,61 @@
 package com.example.ruslaniusupov.joketelling;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Parcelable;
+import android.support.constraint.Group;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.ruslaniusupov.displayjoke.JokeActivity;
+import com.example.ruslaniusupov.displayjoke.model.Joke;
+import com.example.ruslaniusupov.jokes.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BUNDLE_JOKES = "jokes";
+
+    private List<Joke> mJokes;
+
+    @BindView(R.id.loading_pb)ProgressBar mLoadingPb;
+    @BindView(R.id.content)Group mContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        if (savedInstanceState == null) {
+
+            new JokesLoaderAsyncTask().execute();
+
+        } else {
+
+            mJokes = savedInstanceState.getParcelableArrayList(BUNDLE_JOKES);
+
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList(BUNDLE_JOKES, (ArrayList<? extends Parcelable>) mJokes);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -41,6 +83,47 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.tell_joke_btn)
     public void tellJoke(View view) {
 
+        if (mJokes != null && mJokes.size() != 0) {
+
+            int length = mJokes.size();
+
+            Random random = new Random();
+            int index = random.nextInt(length-1);
+            Joke joke = mJokes.get(index);
+
+            Intent launchJokeActivity = new Intent(this, JokeActivity.class);
+            launchJokeActivity.putExtra(JokeActivity.EXTRA_JOKE, joke);
+            startActivity(launchJokeActivity);
+
+        } else {
+            Toast.makeText(this, getString(R.string.no_joke_msg), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private class JokesLoaderAsyncTask extends AsyncTask<Void, Void, List<Joke>> {
+
+        @Override
+        protected List<Joke> doInBackground(Void... voids) {
+            showProgressBar();
+            return Utils.getJokes(DataProvider.getJokesJson());
+        }
+
+        @Override
+        protected void onPostExecute(List<Joke> jokes) {
+            hideProgressBar();
+            mJokes = jokes;
+        }
+    }
+
+    private void showProgressBar() {
+        mContent.setVisibility(View.GONE);
+        mLoadingPb.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mLoadingPb.setVisibility(View.GONE);
+        mContent.setVisibility(View.VISIBLE);
     }
 
 }
---
+++ displayjoke/src/main/java/com/example/ruslaniusupov/displayjoke/JokeActivity.java
@@ -0,0 +1,63 @@
+package com.example.ruslaniusupov.displayjoke;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.ruslaniusupov.displayjoke.model.Joke;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String EXTRA_JOKE = "extra_joke";
+    public static final String BUNDLE_JOKE = "bundle_joke";
+
+    private Joke mJoke;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        if (savedInstanceState == null) {
+
+            Intent intent = getIntent();
+
+            if (intent.hasExtra(EXTRA_JOKE)) {
+                mJoke = intent.getParcelableExtra(EXTRA_JOKE);
+                updateUi();
+            }
+
+        } else {
+
+            mJoke = savedInstanceState.getParcelable(BUNDLE_JOKE);
+            updateUi();
+
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(BUNDLE_JOKE, mJoke);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void updateUi() {
+
+        if (mJoke == null) {
+            return;
+        }
+
+        TextView categoryTv = findViewById(R.id.category_tv);
+        TextView jokeTv = findViewById(R.id.joke_tv);
+        RatingBar ratingBar = findViewById(R.id.rating);
+
+        categoryTv.setText(mJoke.getCategory());
+        jokeTv.setText(mJoke.getBody());
+        ratingBar.setRating(mJoke.getRating());
+
+    }
+
+}
